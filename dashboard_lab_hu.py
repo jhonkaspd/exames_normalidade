642e324cac88b02c2e54657dbec9a3128745c789
jhonkaspd <jcpdfar@gmail.com>
2026-04-11 22:24:43 -0300

    Layout 3.3

--- dashboard_lab_hu.py
+++ dashboard_lab_hu.py
@@ -1355,7 +1355,7 @@ with tab3:
                 x_pct = 0.11
 
                 # início do gráfico
-                x_domain_inicio = 0.13
+                x_domain_inicio = 0.15
 
                 # margem esquerda em px — corrigida
                 margem_esquerda = min(max(120 + max_exame * 7, 220), 20)
